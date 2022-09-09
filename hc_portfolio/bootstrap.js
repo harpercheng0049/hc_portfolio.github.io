125d19e9e4509791201c3246361c299a5dad141a
harpercheng0049 <69156303+harpercheng0049@users.noreply.github.com>
2022-09-09 13:40:29 +0800

    version_1

---
+++ hc_portfolio/bootstrap.js
@@ -0,0 +1,9 @@
+let nav = document.querySelector("nav");
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 100) {
+    nav.classList.add("shadow");
+  } else {
+    nav.classList.remove("shadow");
+  }
+});
